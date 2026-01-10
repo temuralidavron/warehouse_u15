8d318b532ec64420c6ad867e03f37dd188c30f68
temurdev <aliyer.temur95@gmail.com>
2026-01-10 17:52:54 +0500

    new

--- user.py
+++ user.py
@@ -1,13 +1,26 @@
+from order import Order
+
+
 class User:
     def __init__(self,id,username,balance):
         self.id=id
         self.username=username
         self.balance=balance
+        self.orders=list()
 
 
     def get_info(self):
         return f"Kampaniya id:{self.id} Nomi {self.username} balance {self.balance}"
 
-#
+    def add_order(self,order):
+        if isinstance(order,Order):
+            self.orders.append(order)
+
+
+    def get_order(self):
+        orders=[o.get_buy() for o in self.orders]
+        return orders
+
+
 # user=User(1,"karzinka",5000)
 # print(user.get_info())
