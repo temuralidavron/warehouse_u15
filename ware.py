57aa50dad16c5b88cf13bb3a7a6cd08b04b9a79a
temurdev <aliyer.temur95@gmail.com>
2026-01-13 17:19:41 +0500

    task future

--- ware.py
+++ ware.py
@@ -25,3 +25,23 @@ class Warehouse:
 # warehouse=Warehouse(1,"karzinka")
 # warehouse.add_cat(cat,cat3,cat2)
 # print(warehouse.get_cats())
+
+# Admin
+# eng ko'p mahsulot for warehouse cats=[1,2,3,3,4]
+# eng kam mahsulot
+# eng qimmat mahuslot
+# eng arzon mahsulot qaysi
+# eng ko'p sotilgan mahsulot
+# eng kam sotilgan mahsulot
+# barcha categorylarda  cat_name mahsulotlar soni
+# jami mahsulotlar soni
+# warehousedagi jami total
+# tovarga bonus qollash
+# katta narxli savdo qilivchi customer sort
+
+# Customer User
+# sotib_olganalarim
+
+#
+# sonlar=[23,3,4,5,6,67,8]
+# print(sorted(sonlar,reverse=True))
